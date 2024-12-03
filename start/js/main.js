2f801af6d98f04bb4d333bad44a862638e0e5228
Кирилл <johndoe@example.com>
2024-12-03 22:08:46 +0300

    first

---
+++ start/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+   $('.bike-slider, .slider_items').slick({
+      arrows: false,
+      dots: true,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+   });
+});
